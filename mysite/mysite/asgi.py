e38f9ea1811070a3a7d7492ad31658ab54e463cf
oliver nadj <mr.oliver.nadj@gmail.com>
2023-04-15 09:15:36 +0200

    Added the final state of https://channels.readthedocs.io/en/stable/tutorial/index.html

---
+++ mysite/mysite/asgi.py
@@ -0,0 +1,20 @@
+# mysite/asgi.py
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
